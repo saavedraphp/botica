84cc8f743f7575e0ba7eee88bc9aa03edef04441
Magda Pacheco <mpacheco@sacramento.local>
2021-06-23 08:42:01 -0500

    20210623

--- app/Http/Controllers/PedidosController.php
+++ app/Http/Controllers/PedidosController.php
@@ -3,8 +3,29 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Exception;
+
 
 class PedidosController extends Controller
 {
-    //
+    public function grabarListaPedido(Request $request)
+    {
+        DB::beginTransaction();
+        try {
+         //        DB::enableQueryLog();
+        $productos = Producto::where('pp_nombre', 'LIKE', '%' . trim($request->input('palabra')). '%')->orderBy('pp_precio', 'asc')->get();
+        
+        //$productos = DB::table('productos_precio')
+          //              ->where('pp_nombre','LIKE','%'.trim($request->palabra).'%')->orderBy('pp_precio', 'asc')->get();
+         //dd(DB::getQueryLog());
+         DB::commit();
+        return $productos;
+        
+        } catch (Exception $e) {
+            DB::rollBack(); 
+            report($e);
+            return $e;
+        }
+
+    }
 }
